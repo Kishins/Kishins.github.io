57b7fdbbd7b61cdbba3a4eb871fb6ae7b4a0f732
Kishins <inglun13@edu.umea.se>
2023-06-07 13:30:59 +0200

    all of the shit

---
+++ docs/_app/immutable/nodes/4.d205d544.js
@@ -0,0 +1,43 @@
+import{S as it,i as st,s as ct,k as e,a as d,q as p,l as r,c as u,m as o,r as m,h as n,n as i,J as dt,b as V,D as t,H as W}from"../chunks/index.3b555952.js";function ut(at){let _,P,c,f,C,O,g,S,B,l,x,Y,N,w,U,q,b,k,X,A,E,I,y,j,G,v,J,R,T,H,z,F,M,K;return{c(){_=e("!DOCTYPE"),P=d(),c=e("html"),f=e("head"),C=e("meta"),O=d(),g=e("title"),S=p("Capitalism"),B=d(),l=e("body"),x=e("h1"),Y=p("The Money Printer"),N=d(),w=e("p"),U=p("This site is for capitalist scum"),q=d(),b=e("button"),k=e("img"),A=d(),E=e("h3"),I=p("Networth: "),y=e("span"),j=p("0"),G=d(),v=e("button"),J=p("Brokie button"),R=d(),T=e("body"),H=e("script"),z=p(`var clicks = 0;\r
+        function increase() {\r
+            clicks = clicks + 1;\r
+            amount.innerHTML = clicks\r
+        }\r
+        function reset() {\r
+            clicks = 0;\r
+            amount.innerHTML = clicks;\r
+        }`),F=d(),M=e("style"),K=p(`body {\r
+            background-color: #99ffcc;\r
+            color: purple;\r
+        }\r
+        #amount {\r
+            color: red;\r
+        }\r
+        #dollar {\r
+            width: 300px;\r
+            length: 200px;\r
+        }\r
+        .reset {\r
+            \r
+        }`),this.h()},l(s){_=r(s,"!DOCTYPE",{html:!0}),P=u(s),c=r(s,"HTML",{lang:!0});var h=o(c);f=r(h,"HEAD",{});var D=o(f);C=r(D,"META",{name:!0,content:!0}),O=u(D),g=r(D,"TITLE",{});var Z=o(g);S=m(Z,"Capitalism"),Z.forEach(n),D.forEach(n),B=u(h),l=r(h,"BODY",{});var a=o(l);x=r(a,"H1",{});var $=o(x);Y=m($,"The Money Printer"),$.forEach(n),N=u(a),w=r(a,"P",{});var tt=o(w);U=m(tt,"This site is for capitalist scum"),tt.forEach(n),q=u(a),b=r(a,"BUTTON",{type:!0,onclick:!0});var et=o(b);k=r(et,"IMG",{src:!0,alt:!0,id:!0}),et.forEach(n),A=u(a),E=r(a,"H3",{});var Q=o(E);I=m(Q,"Networth: "),y=r(Q,"SPAN",{id:!0});var rt=o(y);j=m(rt,"0"),rt.forEach(n),Q.forEach(n),G=u(a),v=r(a,"BUTTON",{id:!0,onclick:!0});var nt=o(v);J=m(nt,"Brokie button"),nt.forEach(n),R=u(a),T=r(a,"BODY",{});var L=o(T);H=r(L,"SCRIPT",{});var lt=o(H);z=m(lt,`var clicks = 0;\r
+        function increase() {\r
+            clicks = clicks + 1;\r
+            amount.innerHTML = clicks\r
+        }\r
+        function reset() {\r
+            clicks = 0;\r
+            amount.innerHTML = clicks;\r
+        }`),lt.forEach(n),F=u(L),M=r(L,"STYLE",{});var ot=o(M);K=m(ot,`body {\r
+            background-color: #99ffcc;\r
+            color: purple;\r
+        }\r
+        #amount {\r
+            color: red;\r
+        }\r
+        #dollar {\r
+            width: 300px;\r
+            length: 200px;\r
+        }\r
+        .reset {\r
+            \r
+        }`),ot.forEach(n),L.forEach(n),a.forEach(n),h.forEach(n),this.h()},h(){i(_,"html",""),i(C,"name","viewport"),i(C,"content","width=device-width, initial-scale=1"),dt(k.src,X="https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/US_one_dollar_bill%2C_obverse%2C_series_2009.jpg/252px-US_one_dollar_bill%2C_obverse%2C_series_2009.jpg")||i(k,"src",X),i(k,"alt","dollar"),i(k,"id","dollar"),i(b,"type","button"),i(b,"onclick","increase()"),i(y,"id","amount"),i(v,"id","reset"),i(v,"onclick","reset()"),i(c,"lang","en")},m(s,h){V(s,_,h),V(s,P,h),V(s,c,h),t(c,f),t(f,C),t(f,O),t(f,g),t(g,S),t(c,B),t(c,l),t(l,x),t(x,Y),t(l,N),t(l,w),t(w,U),t(l,q),t(l,b),t(b,k),t(l,A),t(l,E),t(E,I),t(E,y),t(y,j),t(l,G),t(l,v),t(v,J),t(l,R),t(l,T),t(T,H),t(H,z),t(T,F),t(T,M),t(M,K)},p:W,i:W,o:W,d(s){s&&n(_),s&&n(P),s&&n(c)}}}class pt extends it{constructor(_){super(),st(this,_,null,ut,ct,{})}}export{pt as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,63 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-import { mdsvex } from 'mdsvex'
-import remarkGithub from 'remark-github';
-import remarkAbbr from 'remark-abbr';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
-
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [preprocess(), mdsvex({
-		extensions: ['.md', '.svx'],
-		layout: { blog: './src/routes/blog/post.svelte' },
-	})],
-
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs',
 		})
-	},
-
-	vitePlugin: {
-		experimental: {
-			inspector: {
-				toggleKeyCombo: 'meta-shift',
-				holdMode: true,
-				showToggleButton: 'always',
-				toggleButtonPos: 'bottom-right'
-			}
-		}
-	},
-
-	extensions: ['.svelte', '.md', '.svx'],
-	remarkPlugins: [
-		[
-			remarkGithub,
-			{
-				// Use your own repository
-				repository: 'https://github.com/Kishins/kishins.github.io.git'
-			}
-		],
-		remarkAbbr
-	],
-	rehypePlugins: [
-		rehypeSlug,
-		[
-			rehypeAutolinkHeadings,
-			{
-				behavior: 'wrap'
-			}
-		]
-	]
-
-};
+	}
+}
 
 export default config;
-
