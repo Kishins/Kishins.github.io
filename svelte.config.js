c541c73108e3550b10b545c3a7c808830c258012
Kishins <inglun13@edu.umea.se>
2023-06-07 12:54:25 +0200

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,63 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex'
+import remarkGithub from 'remark-github';
+import remarkAbbr from 'remark-abbr';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess(), mdsvex({
+		extensions: ['.md', '.svx'],
+		layout: { blog: './src/routes/blog/post.svelte' },
+	})],
+
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+		})
+	},
+
+	vitePlugin: {
+		experimental: {
+			inspector: {
+				toggleKeyCombo: 'meta-shift',
+				holdMode: true,
+				showToggleButton: 'always',
+				toggleButtonPos: 'bottom-right'
+			}
+		}
+	},
+
+	extensions: ['.svelte', '.md', '.svx'],
+	remarkPlugins: [
+		[
+			remarkGithub,
+			{
+				// Use your own repository
+				repository: 'https://github.com/Kishins/kishins.github.io.git'
+			}
+		],
+		remarkAbbr
+	],
+	rehypePlugins: [
+		rehypeSlug,
+		[
+			rehypeAutolinkHeadings,
+			{
+				behavior: 'wrap'
+			}
+		]
+	]
+
 };
 
 export default config;
+
